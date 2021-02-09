865cb28c53f1d1fdfb082fe4c739051664fdf8c6
Nishtha <nishtha.cfa@smartservice.qld.gov.au>
2021-02-10 02:11:52 +1100

    updated footer

--- react-website/src/components/pages/VerdantU.js
+++ react-website/src/components/pages/VerdantU.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../App.css';
 import { Link } from 'react-router-dom';
 import 'react-awesome-slider/dist/styles.css';
+import Footer from '../Footer';
 
 export default function FoodLabel() {
   return  (
@@ -10,6 +11,7 @@ export default function FoodLabel() {
       <i class="fa fa-android"></i>
       <h1>Download Android App</h1>
     </Link>
+    <Footer/>
     </div>
   );
 }
